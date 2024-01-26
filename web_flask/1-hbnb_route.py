233ba33e4256bd38874a9aa4df404516dae7b5d4
AymanSdk <ayman.sdk123@oulook.com>
2024-01-26 17:56:05 +0100

    Refactor Flask routes in 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-# Start a Flask web application:
+""" Script to starts a Flask application  on 0.0.0.0 port 5000 """
+
 from flask import Flask
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """Print Hello HBNB!"""
+@app.route('/')
+def hello_hbnb():
+    """ Hello HBNB! """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """Print HBNB"""
+    """ HBNB """
     return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0 port=5000', debug=True)
