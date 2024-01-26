8cc472bf97a5125b42ba989ee7007ad6ad82805c
AymanSdk <ayman.sdk123@oulook.com>
2024-01-26 17:46:59 +0100

    Update Flask application startup message

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application  listening on 0.0.0.0 port 5000 """
+""" Script to starts a Flask application  on 0.0.0.0 port 5000 """
 from flask import Flask
 app = Flask(__name__)
 
@@ -11,4 +11,4 @@ def hello_hbnb():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000 , debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
