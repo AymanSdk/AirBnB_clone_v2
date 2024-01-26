616f7123a6bf7937b4ba62e73d5a37b5ef5d83ee
AymanSdk <ayman.sdk123@oulook.com>
2024-01-26 13:13:34 +0100

    Add strict_slashes=False to root route and enable debug mode

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
     """Print Hello HBNB!"""
     return "Hello HBNB!"
@@ -18,5 +18,5 @@ def hbnb():
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
